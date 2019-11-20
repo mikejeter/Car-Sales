0222f6239a8498bc8495d6a7cc93d031d6225fe2
mikejeter <mjeter45@yahoo.com>
2019-11-19 22:10:17 -0500

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import { createStore, compose } from 'redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const store = createStore(rootReducer, composeEnhancers());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
