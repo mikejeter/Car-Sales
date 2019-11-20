36ef07bbcfcd0f0e4b5fefde881ec03588006f35
mikejeter <mjeter45@yahoo.com>
2019-11-19 23:45:24 -0500

    Created actionTypes.js, actions/index.js, Modified AddedFeature.js, AddedFeatures.js, AdditionalFeature.js, AdditionalFeatures.js

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import * as types from '../actionTypes';
+
+export const removeFeature = item => ({
+  type: types.REMOVE_FEATURE,
+  payload: item
+});
+
+export const buyItem = item => ({
+  type: types.BUY_ITEM,
+  payload: item
+});
